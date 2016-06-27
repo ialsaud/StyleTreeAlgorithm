c1e1d82dbf607fb9a46578a35a93b336ca5d17b0
Salmannm94 <Salmannm94@Gmail.com>
2016-06-26 23:55:52 -0400

    Merge branch 'master' of https://github.com/ialsaud/StyleTreeAlgorithm
    # Conflicts:
    #	PST.py

--- Node.py
+++ Node.py
@@ -1,38 +1,59 @@
 
 class Node:
-    def __init__(self, tag, attrs, children):
+
+    """
+    initializes the node with the required data
+    """
+    def __init__(self, tag, attrib, children, content):
         self.tag = tag
-        self.attrs = attrs
+        self.attrib = attrib
+        self.content = content
         self.children = children
 
+    """
+    prints the tree with indentations to indicate depth or level
+    """
     def __repr__(self, level=0):
         ret = ("\t"*level) + self.print_node() + "\n"
-        for child in list(self.children[1]):
+        for child in self.children[1]:
             ret += child.__repr__(level+1)
         return ret
 
-    # overrides the '==' function.
-    # note that this will go over
-    # the children and will check
-    # their equality to each other.
+    """
+    overrides the '==' function.
+    note: that this will go over the children and will check their equality to each other.
+    """
     def __eq__(self, other):
         if self.tag != other.tag:
             return False
-        if not (compare_dictionaries(self.attrs, other.attrs)):
+        if not (compare_dictionaries(self.attrib, other.attrs)):
             return False
+        """
         for child1, child2 in zip(self.children[1], other.children[1]):
-            if child1.__ne__(child2):
+            if child1.__ne__(child2):  ## TODO should we go all the way down the tree
                 return False
+        """
         return True
-
-    # not equal method
+    """
+    not equal method
+    """
     def __ne__(self, other):
         return self.__eq__(other)
 
+    """
+    this methods can be modified to print node data (tag, attributes, or children set)
+    """
     def print_node(self):
-        return "(%s)" % self.tag
+        children =[]
+        for child in self.children[1]:
+            children.append(child.tag)
+        count = self.children[0]
 
+        return "(%s, %s)" % (self.tag, self.content)
 
+    """
+    this method compares the node attribute dictionaries
+    """
 def compare_dictionaries(dict1, dict2):
     for key in set(dict1.keys()).union(dict2.keys()):
         if key not in dict1:
--- PST.py
+++ PST.py
@@ -3,77 +3,108 @@ import urllib2
 from lxml import html
 
 
-# This module contains the building blocks of the page Style Tree.
 class PST():
+    """
+    # This module contains the building blocks of the page Style Tree.
+    """
     def __init__(self, url):
         page_string = urllib2.urlopen(url)
-        root = html.fromstring(page_string.read()) # this is uniform
+        root = html.fromstring(page_string.read())
         self.root = self.build(root)
 
-    def build(self, root): # TODO we need to make this uniform\\ all good
+    """
+    builds the PST recursively
+    """
+    def build(self, root):
         children = []
         for e in root:
             new_child = self.build(e)
-            children.append(new_child)
+            if self.is_comment(new_child) is False: ## add to children if it's not comment node only
+                children.append(new_child)
         children_ = [1,children]
-        return Node(root.tag, root.attrib, children_)
+
+        if root.text is not None:
+            root.text = root.text.encode('ascii', 'ignore').decode('ascii') ##some of the text provided mosut be encoded
+        else: ## Replace the None content with empty string
+            root.text = ''
+
+        return Node(root.tag, root.attrib, children_, root.text)
+
+    """
+    checks whether the node is a comment
+    """
+    def is_comment(self, node):
+        w = str(node.tag).split()
+        if w[0] == "<cyfunction":
+            return True
+        else:
+            return False
 
 
+########## SST stuf ############
 def increment(children_set):
     children_set[0] += 1
 
+"""
+def BuildSST(SST, root2): ## this should be an SST with a PST
 
-def comparator(root1, root2):
-    if root1.children == root2.children_set2: ## TODO change compare method
-        increment(root1.children)
+    if SST.children == None:
+
+
+    if root1.children[1] == root2.children[1]:
+        root1.children = [[1]]
     elif root1.children != root2.children:
-        root1.children = [root1.children, root2.children] ## does this make a copy of children_set2
-
+        root1.children.append(root2.children) ## does this make a copy of children_set2
     for child1, child2 in zip(root1.children[1], root1.children[1]): ##loops in parallel
         comparator(child1, child2)
+"""
 
 
+
+
+
+
+
+########## TEST ############
 x = PST('http://www.securityfocus.com/bid/83265')
 x2 = PST('http://www.securityfocus.com/bid/83265')
-y = PST('http://www.securityfocus.com/bid/77216')
+y = PST('https://3.basecamp.com/3273604/projects/481759')
 z = PST('http://www.securityfocus.com/bid/69077')
 
-print x.root == y.root ## yeap it works.
-
-
-
-
-# print (x.root)
-#
-# comparator(x.root,y.root)
-#
-#
-# print x.root
+print x.root
+SST = Node(x.root.tag, x.root.tag, None, x.root.text) ## empty node with the same information as one of the roots (all roots all the same)
+print x.node
 
 
 
 
 
 
-# ==== Scratch =====
+########## Scratch ##########
 
 """
-        #SKRATCH
-        #from lxml import html
-        #import urllib2
+        print (x.root)
 
-        #A = urllib2.urlopen("url")
-        #root = html.from_string(A.read())
-        #root.text_content #prints the whole pag
+        comparator(x.root,y.root)
+
+        print x.root
+
+"""
+
+"""
+        from lxml import html
+        import urllib2
+
+        A = urllib2.urlopen("url")
+        root = html.from_string(A.read())
+        root.text_content #prints the whole pag
 """
 
 """
         import inspect
         print inspect.getargspec(fromstring)
         shows the arguments of a method
-        """
 
-"""
         dir(fromstring)
         shows the methods
 """
